30cbb2586168264e54b7c83ca2a207b59818295a
jeroen <jeroeningelbrecht@hotmail.com>
2019-07-22 00:28:58 +0200

    first successful request to love-calculator

---
+++ be/naaipoes/main/driver.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get('https://love-calculator.p.rapidapi.com/getPercentage',
+                        params={'fname': 'Jeroen', 'sname': 'Taike'},
+                        headers={
+                            "X-RapidAPI-Host": "love-calculator.p.rapidapi.com",
+                            "X-RapidAPI-Key": ""
+                        }
+                        )
+
+print(response)
